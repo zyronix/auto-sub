8139c8d965ed606284f48e014cbb727cfac62846
mintje <mintje@localhost>
2011-09-13 21:37:20 +0200

    Cleaned up the archive Splitted the main Autosub_dev file into multiple modules:  Autosub.py - main program  Bierdopje.py - the bierdopje api abstraction layer  Helpers.py - generic helper functions  LocalDisk.py - module for the local disk IO  Config.py - Class and functions to carry the various settings and properties accross the file

---
+++ Config.py
@@ -0,0 +1,116 @@
+# Autosub Config.py - http://code.google.com/p/auto-sub/
+#
+# The Autosub config Module
+# 
+
+import urllib
+import urllib2
+import os
+import re
+import logging
+
+from ConfigParser import SafeConfigParser
+
+# Autosub specific modules
+
+# Settings -----------------------------------------------------------------------------------------------------------------------------------------
+# Location of the configuration file:
+configfile = "config.properties"
+# Set the logger
+log = logging.getLogger('thelogger')
+#/Settings -----------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Properties():
+	
+	# Read config file
+	rootpath = os.getcwd()
+	fallbackToEng = True
+	subeng="en"
+	logfile="AutoSubService.log"
+	edited=False
+		
+	cfg = SafeConfigParser()
+	cfg.read(configfile)
+
+	try:
+		dict(cfg.items('config'))
+	except:
+		cfg.add_section('config')
+		edited=True
+	try:    
+		rootpath=cfg.get("config", "ROOTPATH")
+	except:
+		cfg.set("config","ROOTPATH",rootpath)
+		edited=True
+	try:
+		fallbackToEng=cfg.getboolean("config", "FALLBACKTOENG")
+	except:
+		cfg.set("config","FALLBACKTOENG",str(fallbackToEng))
+		edited=True
+	try:    
+		subeng=cfg.get("config", "SUBENG")
+	except:
+		cfg.set("config","SUBENG",subeng)
+		edited=True
+	try:
+		logfile= cfg.get("config", "LOGFILE")
+	except:
+		cfg.set("config","LOGFILE",logfile) 
+		edited=True
+	
+	skipshow={}
+	
+	try:
+	# Try to read skipshow section in the config
+		skipshow = dict(cfg.items('skipshow'))
+	except:
+		cfg.add_section('skipshow')
+		edited=True  
+	if edited:
+		with open(configfile, 'wb') as file:
+			cfg.write(file)
+
+	# Try to read skipshow section in the config
+	skipshow = dict(cfg.items('skipshow'))
+
+	# The following 4 lines convert the skipshow to uppercase. And also convert the variables to a list 
+	skipshowupper = {}
+	for x in skipshow:
+		skipshowupper[x.upper()] = skipshow[x].split(',')
+	
+	# Settings
+	showid_cache = {}
+
+	# This dictionary maps local series names to BierDopje ID's
+	# Example: namemapping = {"Castle":"12708"}
+	namemapping = {
+			"Greys Anatomy" : "3733",
+			"Grey's Anatomy" : "3733",
+			"Csi Miami" : "2187",
+			"Mr Sunshine":"14224",
+			"Spartacus Gods Of The Arena":"14848",
+			"Spartacus Blood And Sand":"13942",
+			"Hawaii Five 0":"14211"
+	}
+	
+def update_showid_cache(new_cache=None):
+	showid_cache = new_cache
+	del new_cache
+	return True
+
+def nameMapping(showName):
+	if showName in Properties.namemapping.keys():
+		log.debug("nameMapping: found match for %s" %showName)
+		return Properties.namemapping[showName]
+		
+def SkipShow(showName,season,episode):
+	if showName.upper() in Properties.skipshowupper.keys():
+		log.debug("SkipShow: Found %s in skipshow dictonary" %showName)
+		for seasontmp in skipshowupper[showName.upper()]:
+			if seasontmp == '0':
+				log.debug("SkipShow: variable of %s is set to 0, skipping the complete Serie" %showName)
+				return True
+			elif int(seasontmp) == int(season):
+				log.debug("SkipShow: Season matches variable of %s, skipping season" %showName)
+				return True	
