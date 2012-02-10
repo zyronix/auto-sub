c9213fb541578ded73ce1f5a24d2a2bb6a3ba722
Bas Roijen <basroijen@gmail.com>
2012-02-10 14:20:11 +0100

    Added docstrings and code cleanup.

--- autosub/checkSub.py
+++ autosub/checkSub.py
@@ -1,3 +1,8 @@
+# Autosub Db.py - http://code.google.com/p/auto-sub/
+#
+# The Autosub checkSub module
+#
+
 import logging
 import os
 
@@ -7,63 +12,68 @@ import autosub.Bierdopje
 # Settings
 log = logging.getLogger('thelogger')
 
+
 class checkSub():
+    """
+    Check the Bierdopje API for subtitles of episodes that are in the WANTEDQUEUE.
+    If the subtitles are found, they are added to the TODOWNLOADQUEUE
+    """
     def run(self):
         log.debug("checkSub: Starting round of checkSub")
         toDelete_wantedQueue = []
-        
+
         if autosub.TODOWNLOADQUEUELOCK or autosub.WANTEDQUEUELOCK:
             log.debug("checkSub: Exiting, another threat is using the queues")
             return False
         else:
-            autosub.TODOWNLOADQUEUELOCK=True
-            autosub.WANTEDQUEUELOCK=True
-        
+            autosub.TODOWNLOADQUEUELOCK = True
+            autosub.WANTEDQUEUELOCK = True
+
         for index, wantedItem in enumerate(autosub.WANTEDQUEUE):
             title = wantedItem['title']
             season = wantedItem['season']
             episode = wantedItem['episode']
             originalfile = wantedItem['originalFileLocationOnDisk']
             language = wantedItem['lang']
-            
-            if autosub.SUBNL!="":
+
+            if autosub.SUBNL != "":
                 srtfile = os.path.join(originalfile[:-4] + "." + autosub.SUBNL + ".srt")
             else:
                 srtfile = os.path.join(originalfile[:-4] + ".srt")
-            
-            engsrtfile = os.path.join(originalfile[:-4] + "."+ autosub.SUBENG +".srt")
-            
+
+            engsrtfile = os.path.join(originalfile[:-4] + "." + autosub.SUBENG + ".srt")
+
             if title in autosub.SHOWID_CACHE.keys():
                 showid = autosub.SHOWID_CACHE[title]
-            
+
             if not title in autosub.SHOWID_CACHE.keys():
                 showid = autosub.Helpers.nameMapping(title)
-                if not showid: 
-                    log.debug("checkSub: no NameMapping found for %s, trying the Bierdopje API" %title)
+                if not showid:
+                    log.debug("checkSub: no NameMapping found for %s, trying the Bierdopje API" % title)
                     showid = autosub.Bierdopje.getShowid(title)
                     if not showid:
-                        log.error("checkSub: Could not find a show ID for %s" %title)
+                        log.error("checkSub: Could not find a show ID for %s" % title)
                         autosub.SHOWID_CACHE[title] = -1
                         continue
-                log.debug("checkSub: Got the following showid: %s" %showid)
+                log.debug("checkSub: Got the following showid: %s" % showid)
                 autosub.SHOWID_CACHE[title] = showid
-            
-            log.debug("checkSub: trying to get a downloadlink for %s, language is %s" % (originalfile,language))
-            
+
+            log.debug("checkSub: trying to get a downloadlink for %s, language is %s" % (originalfile, language))
+
             downloadLink = autosub.Bierdopje.getSubLink(showid, language, wantedItem)
-            
+
             if not downloadLink and language == 'nl' and not autosub.DOWNLOADENG:
                 if autosub.FALLBACKTOENG == False:
-                    log.debug("checkSub: No dutch subtitles found on bierdopje.com for %s - Season %s - Episode %s" %(title, season,episode))
+                    log.debug("checkSub: No dutch subtitles found on bierdopje.com for %s - Season %s - Episode %s" % (title, season, episode))
                     continue
                 if os.path.exists(engsrtfile) and autosub.FALLBACKTOENG == True:
-                    log.info("checkSub: The english subtitle is found, will try again later for the dutch version %s - Season %s - Episode %s" %(title, season,episode))
+                    log.info("checkSub: The english subtitle is found, will try again later for the dutch version %s - Season %s - Episode %s" % (title, season, episode))
                     continue
                 if not os.path.exists(engsrtfile) and autosub.FALLBACKTOENG == True:
-                    log.debug("checkSub: Dutch subtitle could not be found on bierdopje.com, checking for the english version for %s - Season %s - Episode %s" %(title, season,episode))
+                    log.debug("checkSub: Dutch subtitle could not be found on bierdopje.com, checking for the english version for %s - Season %s - Episode %s" % (title, season, episode))
                     downloadLink = autosub.Bierdopje.getSubLink(showid, "en", wantedItem)
                     if not downloadLink:
-                        log.info("checkSub: No english subtitles found on bierdopje.com for %s - Season %s - Episode %s" %(title, season,episode))
+                        log.info("checkSub: No english subtitles found on bierdopje.com for %s - Season %s - Episode %s" % (title, season, episode))
                         continue
                     elif downloadLink:
                         wantedItem['downloadLink'] = downloadLink
@@ -71,9 +81,9 @@ class checkSub():
                         autosub.TODOWNLOADQUEUE.append(wantedItem)
                         continue
             elif not downloadLink and language == 'en':
-                log.info('checkSub: no english subtitle found on bierdopje.com for %s - Season %s - Episode %s' %(title, season,episode))
+                log.info('checkSub: no english subtitle found on bierdopje.com for %s - Season %s - Episode %s' % (title, season, episode))
                 continue
-            
+
             elif downloadLink:
                 wantedItem['downloadLink'] = downloadLink
                 if language == 'nl':
@@ -81,19 +91,18 @@ class checkSub():
                 elif language == 'en':
                     wantedItem['destinationFileLocationOnDisk'] = engsrtfile
                 autosub.TODOWNLOADQUEUE.append(wantedItem)
-                log.info("checkSub: The episode %s - Season %s Episode %s has a matching subtitle on bierdopje, adding to toDownloadQueue" %(title,season,episode))
-                log.debug("checkSub: destination filename %s" %wantedItem['destinationFileLocationOnDisk'])
+                log.info("checkSub: The episode %s - Season %s Episode %s has a matching subtitle on bierdopje, adding to toDownloadQueue" % (title, season, episode))
+                log.debug("checkSub: destination filename %s" % wantedItem['destinationFileLocationOnDisk'])
                 toDelete_wantedQueue.append(index)
-                log.debug("checkSub: Removed item: %s from the wantedQueue at index %s" %(wantedItem, index))
-        
-        
-        i=len(toDelete_wantedQueue)-1
-        while i >= 0: 
-            log.debug("checkSub: Removed item from the wantedQueue at index %s" %toDelete_wantedQueue[i])
+                log.debug("checkSub: Removed item: %s from the wantedQueue at index %s" % (wantedItem, index))
+
+        i = len(toDelete_wantedQueue) - 1
+        while i >= 0:
+            log.debug("checkSub: Removed item from the wantedQueue at index %s" % toDelete_wantedQueue[i])
             autosub.WANTEDQUEUE.pop(toDelete_wantedQueue[i])
-            i=i-1
-        
-        log.debug("checkSub: Finished round of checkSub")    
-        autosub.TODOWNLOADQUEUELOCK=False
-        autosub.WANTEDQUEUELOCK=False
+            i = i - 1
+
+        log.debug("checkSub: Finished round of checkSub")
+        autosub.TODOWNLOADQUEUELOCK = False
+        autosub.WANTEDQUEUELOCK = False
         return True
--- autosub/downloadSubs.py
+++ autosub/downloadSubs.py
@@ -1,3 +1,8 @@
+# Autosub Db.py - http://code.google.com/p/auto-sub/
+#
+# The Autosub downloadSubs module
+#
+
 import logging
 import urllib2
 
@@ -5,7 +10,11 @@ import autosub
 
 log = logging.getLogger('thelogger')
 
+
 class downloadSubs():
+    """
+    Check the TODOWNLOADQUEUE and try to download everything in it.
+    """
     def run(self):
         if len(autosub.TODOWNLOADQUEUE) > 0:
             toDelete_toDownloadQueue = []
@@ -14,53 +23,53 @@ class downloadSubs():
                 log.debug("downloadSubs: Exiting, another threat is using the queues")
                 return False
             else:
-                autosub.TODOWNLOADQUEUELOCK=True
-                autosub.WANTEDQUEUELOCK=True
+                autosub.TODOWNLOADQUEUELOCK = True
+                autosub.WANTEDQUEUELOCK = True
             for index, downloadItem in enumerate(autosub.TODOWNLOADQUEUE):
                 if 'destinationFileLocationOnDisk' in downloadItem.keys() and 'downloadLink' in downloadItem.keys():
                     destsrt = downloadItem['destinationFileLocationOnDisk']
                     downloadLink = downloadItem['downloadLink']
-            
+
                     try:
-                        response = urllib2.urlopen(downloadLink)                
-                    except:  
-                        log.error("downloadSubs: The server returned an error for request %s" %downloadLink)
-                        continue
-                    
-                    try:
-                        open(destsrt,'w').write(response.read())
+                        response = urllib2.urlopen(downloadLink)
                     except:
-                        log.error("downloadSubs: Error while writing subtitle file. Destination: %s" %destsrt)
+                        log.error("downloadSubs: The server returned an error for request %s" % downloadLink)
                         continue
-                    
-                    log.info("downloadSubs: DOWNLOADED: %s" %destsrt)
+
+                    try:
+                        open(destsrt, 'w').write(response.read())
+                    except:
+                        log.error("downloadSubs: Error while writing subtitle file. Destination: %s" % destsrt)
+                        continue
+
+                    log.info("downloadSubs: DOWNLOADED: %s" % destsrt)
                     toDelete_toDownloadQueue.append(index)
-                    
+
                     if len(autosub.LASTESTDOWNLOAD) >= 10:
                         autosub.LASTESTDOWNLOAD.pop(0)
                         autosub.LASTESTDOWNLOAD.append(autosub.TODOWNLOADQUEUE[index])
                     else:
                         autosub.LASTESTDOWNLOAD.append(autosub.TODOWNLOADQUEUE[index])
-                    
+
                     if autosub.POSTPROCESSCMD:
                         postprocesscmdconstructed = autosub.POSTPROCESSCMD + ' "' + downloadItem["destinationFileLocationOnDisk"] + '" "' + downloadItem["originalFileLocationOnDisk"] + '"'
-                        log.debug("downloadSubs: Postprocess: running %s" %postprocesscmdconstructed)
+                        log.debug("downloadSubs: Postprocess: running %s" % postprocesscmdconstructed)
                         log.info("downloadSubs: Running PostProcess")
-                        postprocessoutput,postprocesserr = autosub.Helpers.RunCmd(postprocesscmdconstructed)
+                        postprocessoutput, postprocesserr = autosub.Helpers.RunCmd(postprocesscmdconstructed)
                         if postprocesserr:
-                            log.error("downloadSubs: PostProcess: %s" %postprocesserr)
-                        log.debug("downloadSubs: PostProcess Output:% s" %postprocessoutput)
-                    
+                            log.error("downloadSubs: PostProcess: %s" % postprocesserr)
+                        log.debug("downloadSubs: PostProcess Output:% s" % postprocessoutput)
+
                     #toDownloadQueue.remove(downloadItem)
                 else:
                     log.error("downloadSub: No downloadLink or locationOnDisk found at downloadItem, skipping")
                     continue
-            
-            i=len(toDelete_toDownloadQueue)-1
-            while i >= 0: 
-                log.debug("downloadSubs: Removed item from the toDownloadQueue at index %s" %toDelete_toDownloadQueue[i])
+
+            i = len(toDelete_toDownloadQueue) - 1
+            while i >= 0:
+                log.debug("downloadSubs: Removed item from the toDownloadQueue at index %s" % toDelete_toDownloadQueue[i])
                 autosub.TODOWNLOADQUEUE.pop(toDelete_toDownloadQueue[i])
-                i=i-1
-            autosub.TODOWNLOADQUEUELOCK=False
-            autosub.WANTEDQUEUELOCK=False
+                i = i - 1
+            autosub.TODOWNLOADQUEUELOCK = False
+            autosub.WANTEDQUEUELOCK = False
         return True
