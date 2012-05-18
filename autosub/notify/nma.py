83dde4b198c54b65aeb21297a3b10c343df1ffdd
Donny <donny.vanosch@gmail.com>
2012-05-18 20:45:24 +0200

    Small modification to notification screen - Disabled Growl Enable/Disable dropdown till it's been implemented. - Added information that config first needs to be saved before you can test. - Grammar fix in NMA module.

--- autosub/notify/nma.py
+++ autosub/notify/nma.py
@@ -21,7 +21,7 @@ def _send_notify(message):
 
 def test_notify():
     log.debug("NMA: Trying to send a notification")
-    message = "Auto-Sub sucessfully sended a test message"
+    message = "Auto-Sub sucessfully sent a test message"
     return _send_notify(message)
         
 def send_notify(lang, subtitlefile, videofile):
